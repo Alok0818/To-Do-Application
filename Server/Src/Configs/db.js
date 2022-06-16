ff47bf84f839af1aa594e56ad13412181d67ee5c
Alok Kushwaha <94324872+Alok0818@users.noreply.github.com>
2022-06-16 23:10:05 +0530

    database created

---
+++ Server/Src/Configs/db.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+return mongoose.connect("mongodb+srv://Alok0818:8308303270@cluster0.pmkry.mongodb.net/To-Do-Application?retryWrites=true&w=majority");
+};
+
+// mongodb+srv://Alok0818:8308303270@cluster0.pmkry.mongodb.net/To-Do-Application?retryWrites=true&w=majority
