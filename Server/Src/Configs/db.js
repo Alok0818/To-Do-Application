ad852ff149fec2fe6317a58e917b6567ad8dbe77
Alok Kushwaha <94324872+Alok0818@users.noreply.github.com>
2023-01-25 16:32:31 +0530

    redeploying project

--- Server/Src/Configs/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = () => {
-return mongoose.connect("mongodb+srv://Alok0818:8308303270@cluster0.pmkry.mongodb.net/To-Do-Application?retryWrites=true&w=majority");
-};
